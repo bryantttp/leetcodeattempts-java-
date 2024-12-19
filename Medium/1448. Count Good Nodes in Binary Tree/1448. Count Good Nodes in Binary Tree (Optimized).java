efcf04d0b56a1261372d6100fa29923a30983531
bryantttp <149286642+bryantttp@users.noreply.github.com>
2024-12-19 17:38:45 +0800

    Update 1448. Count Good Nodes in Binary Tree (Optimized).java

--- Medium/1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree (Optimized).java
+++ Medium/1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree (Optimized).java
@@ -1,1 +1,37 @@
 // 191224 Submission
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    private int dfs(TreeNode node, int maxVal) {
+        if (node == null) {
+            return 0;
+        }
+
+        int res = (node.val >= maxVal) ? 1 : 0;
+        maxVal = Math.max(maxVal, node.val);
+        res += dfs(node.left, maxVal);
+        res += dfs(node.right, maxVal);
+        return res;
+    }
+    // Runtime = 2ms, beats 85.39% of users
+    // Memory = 52.68 MB, beats 25.01% of users
+
+    // No global variable, count is being stored when dfs ends
+}
